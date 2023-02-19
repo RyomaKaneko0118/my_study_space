309649528b4ac9bb8b9fe132f342c7d484956bd0
RyomaKaneko <banzai.nippon2030@gmail.com>
2023-02-19 12:19:56 +0900

    WIP: インポートとエキスポート

--- 0218_1/src/main.ts
+++ 0218_1/src/main.ts
@@ -1,3 +1,6 @@
-import { message } from "./message/attribute";
+import { message } from "./message/attribute"
+import { age, getName, name } from "./user/attribute"
 
 console.log(message)
+console.log(name, age)
+console.log(getName())
---
+++ 0218_1/src/user/attribute.ts
@@ -0,0 +1,6 @@
+export const name = "Alice"
+export const age = 36
+
+export const getName = () => {
+  return name    
+}
