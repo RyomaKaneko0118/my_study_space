efe99a8ecc0adf136e7cca615bab8a983ecbe523
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-02-23 17:10:22 +0900

    メッセージの出力をリファクタリング

--- python/0223_1/array_1.py
+++ python/0223_1/array_1.py
@@ -2,7 +2,8 @@ subject = ["国語", "数学", "英語", "理科", "社会"]
 score = [80, 100, 92, 96, 74]
 total = 0
 for i in range(5):
-    print(subject[i], "の点数は", score[i])
+    message = f"{subject[i]}の点数は{score[i]}"
+    print(message)
     total += score[i]
 
 print("合格点は", total)
