50845c9be3e7b355aa2de95a4cb12901b79ebae2
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-02-23 16:53:51 +0900

    pythonにおける配列のサンプル

--- python/0223_1/array_1.py
+++ python/0223_1/array_1.py
@@ -1,1 +1,8 @@
-print("Hello")
+subject = ["国語", "数学", "英語", "理科", "社会"]
+score = [80, 100, 92, 96, 74]
+total = 0
+for i in range(5):
+    print(subject[i], "の点数は", score[i])
+    total += score[i]
+
+print("合格点は", total)
