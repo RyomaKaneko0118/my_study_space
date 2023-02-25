c97522b162086eb888b34571fca7b136097a114e
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-02-25 19:56:17 +0900

    例:双方向ノード

---
+++ python/0223_1/graph_4.py
@@ -0,0 +1,22 @@
+data = [
+    [0, 1, 0, 0, 0],
+    [1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0]
+]
+node = [
+    "(0)", "(1)", "(2)", "(3)", "(4)"
+]
+arrow = ["", "-->", "<--", "<->",]
+
+for y in range(5):
+    for x in range(y, 5):
+        edge1 = data[y][x]
+        edge2 = data[x][y]
+        direction = edge1 + edge2 * 2
+
+        if direction > 0:
+            message = f"{node[y]} {arrow[direction]} {node[x]}"
+            print(message)
+        
