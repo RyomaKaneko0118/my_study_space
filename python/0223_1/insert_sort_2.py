19ea88aef8803fac23158aee105325ab47266596
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-02-26 11:34:35 +0900

    データを乱数にする

---
+++ python/0223_1/insert_sort_2.py
@@ -0,0 +1,20 @@
+import random
+MAX = 15
+data = [0] * MAX
+
+for i in range(MAX):
+    data[i] = random.randint(i, 99)
+
+message = f"元のデータ: {data}"
+print(message)
+
+for i in range(1, MAX):
+    tmp = data[i]
+    j = i
+
+    while j > 0 and data[j -1] > tmp:
+        data[j] = data[j -1]
+        j = j -1
+    data[j] = tmp
+message = f"ソート後のデータ: {data}"
+print(message)
