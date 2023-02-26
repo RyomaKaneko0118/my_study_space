c89eff4c6f08b60bc1a9b3ddac7bd46c78f420c5
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-02-26 10:46:05 +0900

    選択ソート

---
+++ python/0223_1/select_sort_1.py
@@ -0,0 +1,17 @@
+data = [45, 78, 71, 14, 1, 9, 65, 85]
+message = f"元のデータ: {data}"
+print(message)
+MAX = len(data)
+
+for i in range(0, MAX -1):
+    m = i
+    for j in range(i +1, MAX):
+        if data[j] < data[m]:
+            m = j
+    
+    print(data)
+    data[i], data[m] = data[m], data[i]
+    print(data)
+    
+message = f"ソート後のデータ: {data}"
+print(message)
