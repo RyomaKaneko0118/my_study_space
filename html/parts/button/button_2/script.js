245c73ba55d55ac1776fdebc73a18e9b1954f5a7
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-03-04 08:38:38 +0900

    ドロップダウンメニューの追加

--- html/parts/button/button_2/script.js
+++ html/parts/button/button_2/script.js
@@ -1,13 +1,12 @@
-const button = document.querySelector('input');
-const button1 = document.getElementById('button_1')
-console.log(button1)
+var button = document.querySelector('input');
+var button1 = document.getElementById('button_1');
+console.log(button1);
 button.addEventListener('click', disableButton);
-
 function disableButton() {
-  button.disabled = true;
-  button.value = 'Disabled';
-  setTimeout(() => {
-    button.disabled = false;
-    button.value = 'Enabled';
-  }, 2000);
+    button.disabled = true;
+    button.value = 'Disabled';
+    setTimeout(function () {
+        button.disabled = false;
+        button.value = 'Enabled';
+    }, 2000);
 }
