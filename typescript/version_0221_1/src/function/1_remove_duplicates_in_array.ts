d79c9a52faa6734c3bf3cc57bff2601bf25632f4
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-03-05 13:07:59 +0900

    オブジェクト配列の重複を削除

--- typescript/version_0221_1/src/function/1_remove_duplicates_in_array.ts
+++ typescript/version_0221_1/src/function/1_remove_duplicates_in_array.ts
@@ -1,6 +1,6 @@
 // 配列の重複を除去する
 
-const arr: string[] = ["a", "b", "c", "d", "d", "c", "e"]
-const uniqueArray = Array.from(new Set(arr));
+export const arr: string[] = ["a", "b", "c", "d", "d", "c", "e"]
+export const uniqueArray = Array.from(new Set(arr));
 
 console.log(uniqueArray)
--- typescript/version_0221_1/src/main.ts
+++ typescript/version_0221_1/src/main.ts
@@ -1,1 +1,2 @@
 import "./function/1_remove_duplicates_in_array"
+import "./function/2_remove_duplicates_in_an_array_of_objects"
