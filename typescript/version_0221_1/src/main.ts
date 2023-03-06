fcb51032d0e019c3b23f33b60cbaba428d29fa0a
ryomakaneko0118 <kaneko@grabss.co.jp>
2023-03-06 16:26:35 +0900

    指定した文字数で文字列を分ける

--- typescript/version_0221_1/src/main.ts
+++ typescript/version_0221_1/src/main.ts
@@ -1,3 +1,4 @@
 import "./function/1_remove_duplicates_in_array"
 import "./function/2_remove_duplicates_in_an_array_of_objects"
 import "./function/3_iterate_array_iterates_with_index"
+import "./function/4_separate_strings_by_number_of_characters"
