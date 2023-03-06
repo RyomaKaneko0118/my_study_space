c3cc6ca52c94e2adb1037ad0280a2361abe9ee6a
ryomakaneko0118 <kaneko@grabss.co.jp>
2023-03-06 16:49:49 +0900

    文字列を同じ文字ごとに分ける

--- typescript/version_0221_1/src/function/4_separate_strings_by_number_of_characters.ts
+++ typescript/version_0221_1/src/function/4_separate_strings_by_number_of_characters.ts
@@ -1,5 +1,5 @@
 // 指定した文字数で文字列を分ける
 export const str = "asdfghjklmnopq";
-export const splitPairs = str.match(/.{6}/g);
+export const splitPairs: string[] = str.match(/.{6}/g);
 
 console.log(splitPairs); // ['as', 'df', 'gh', 'jk', 'lm', 'no', 'pq']
