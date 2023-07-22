97bce6488fc6ac8bbba537ad3682a2191e1b9f2c
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-07-23 06:23:08 +0900

    Add

--- css/グローバルナビゲーション/追従メニュー/main.js
+++ css/グローバルナビゲーション/追従メニュー/main.js
@@ -4,13 +4,8 @@ let elemTop = [];
 //現在地を取得するための設定を関数でまとめる
 function PositionCheck(){
     //headerの高さを取得
-	// var headerH = $("#header").outerHeight(true);
   const headerH = document.getElementById("header").offsetHeight
-  console.log(headerH)
     //.scroll-pointというクラス名がついたエリアの位置を取得する設定
-	// $(".scroll-point").each(function(i) {//.scroll-pointクラスがついたエリアからトップまでの距離を計算して設定
-	// 	elemTop[i] =Math.round(parseInt($(this).offset().top-headerH));//追従するheader分の高さ（70px）を引き小数点を四捨五入
-	// });
   const scrollPoints = document.querySelectorAll(".scroll-point")
   scrollPoints.forEach(function(elem, i) { elemTop[i] = Math.round(parseInt(elem.offsetTop - headerH)) })
   console.log(elemTop)
@@ -18,48 +13,63 @@ function PositionCheck(){
 
 //ナビゲーションに現在地のクラスをつけるための設定
 function ScrollAnime() {//スクロールした際のナビゲーションの関数にまとめる
-	// var scroll = Math.round($(window).scrollTop());
   let scroll = Math.round(window.scrollY)
-  console.log(scroll)
-	var NavElem = $("#g-nav li");//ナビゲーションのliの何番目かを定義するための準備
-	$("#g-nav li").removeClass('current');//全てのナビゲーションの現在地クラスを除去
-	if(scroll >= 0 && scroll < elemTop[1]) {//スクロール値が0以上 .scroll-point 1つめ（area-1）の高さ未満
-      $(NavElem[0]).addClass('current');//1つめのliに現在地クラスを付与
+  //ナビゲーションのliの何番目かを定義するための準備
+	let NavElem = document.querySelectorAll("#g-nav li")
+  NavElem.forEach(function(element) {
+    element.classList.remove('current');
+  })
+	if(scroll >= 0 && scroll < elemTop[1]) {
+      NavElem[0].classList.add('current')
     }
-	else if(scroll >= elemTop[1] && scroll < elemTop[2]) {//.scroll-point 1つめ（area-1）以上.scroll-point 2つめ（area-2）未満
-     $(NavElem[1]).addClass('current');//2つめのliに現在地クラスを付与
+	else if(scroll >= elemTop[1] && scroll < elemTop[2]) {
+    NavElem[1].classList.add('current')
     } 
-    else if(scroll >= elemTop[2] && scroll < elemTop[3]) {//.scroll-point 2つめ（area-2）以上.scroll-point 3つめ（area-3）未満
-     $(NavElem[2]).addClass('current');//3つめのliに現在地クラスを付与
-    } 
-    else if(scroll >= elemTop[3]) {// .scroll-point 3つめ（area-3）以上
-      $(NavElem[3]).addClass('current');//4つめのliに現在地クラスを付与
-    } 
+  else if(scroll >= elemTop[2] && scroll < elemTop[3]) { 
+    NavElem[2].classList.add('current')
+  } 
+  else if(scroll >= elemTop[3]) {// .scroll-point 3つめ（area-3）以上
+    NavElem[3].classList.add('current')
+  } 
 }
 
-//ナビゲーションをクリックした際のスムーススクロール
-$('#g-nav a').click(function () {
-	var elmHash = $(this).attr('href'); //hrefの内容を取得
-	var headerH = $("#header").outerHeight(true);//追従するheader分の高さ（70px）を引く
-	var pos = Math.round($(elmHash).offset().top-headerH);	//headerの高さを引き小数点を四捨五入
-	$('body,html').animate({scrollTop: pos}, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
-	return false;//リンクの無効化
+// ナビゲーションをクリックした際のスムーススクロール
+// $('#g-nav a').click(function () {
+// 	var elmHash = $(this).attr('href'); //hrefの内容を取得
+// 	var headerH = $("#header").outerHeight(true);//追従するheader分の高さ（70px）を引く
+// 	var pos = Math.round($(elmHash).offset().top-headerH);	//headerの高さを引き小数点を四捨五入
+// 	$('body,html').animate({scrollTop: pos}, 500);//取得した位置にスクロール※数値が大きいほどゆっくりスクロール
+// 	return false;//リンクの無効化
+// });
+
+document.querySelectorAll('#g-nav a').forEach(function(aElement) {
+  aElement.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const targetElement = document.querySelector(this.getAttribute('href'))
+    const headerH = document.querySelector("#header").offsetHeight
+    const pos = Math.round(targetElement.offsetTop - headerH)
+
+    window.scrollTo({
+      top: pos,
+      behavior: 'smooth'
+    });
+  });
 });
 
-
 // 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
+window.addEventListener('scroll', function() {
 	PositionCheck();/* 現在地を取得する関数を呼ぶ*/
 	ScrollAnime();/* ナビゲーションに現在地のクラスをつけるための関数を呼ぶ*/
 });
 
 // ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
+window.addEventListener('load', function () {
 	PositionCheck();/* 現在地を取得する関数を呼ぶ*/
 	ScrollAnime();/* ナビゲーションに現在地のクラスをつけるための関数を呼ぶ*/
 });
 
-$(window).resize(function() {
+window.addEventListener('resize', function() {
   //リサイズされたときの処理
   PositionCheck()
 });
