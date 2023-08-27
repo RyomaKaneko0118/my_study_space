6c4a53a853e6a472dc58e9be689ee046f9c6ff24
RyomaKaneko0118 <kaneko@grabss.co.jp>
2023-08-27 12:55:23 +0900

    Add: サンプルの追加

---
+++ javascript/typescript/environment/src/sample/11_technic/10_.ts
@@ -0,0 +1,11 @@
+export {}
+//longhand
+console.log("check")
+const x: string = "ghi"
+if (x === 'abc' || x === 'def' || x === 'ghi' || x ==='jkl') {
+  console.log(x)
+}
+
+if (['abc', 'def', 'ghi', 'jkl'].includes(x)) {
+  console.log(x)
+}
