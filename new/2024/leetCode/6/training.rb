8178ba7b1bc5fb63bd4074d65885a399ac8e220c
RyomaKaneko0118 <kaneko@grabss.co.jp>
2024-08-21 22:46:04 +0900

    add

--- new/2024/leetCode/6/training.rb
+++ new/2024/leetCode/6/training.rb
@@ -10,8 +10,8 @@
 # @param {ListNode} list2
 # @return {ListNode}
 def merge_two_lists(list1, list2)
-  return list2 if list1.nil?
   return list1 if list2.nil?
+  return list2 if list1.nil?
 
   if list1.val < list2.val
     list1.next = merge_two_lists(list1.next, list2)
